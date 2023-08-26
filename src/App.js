a8ca0560fd74409cc21d39ee8e5b883137162883
Kelvy Kellysson Araújo de Mendonça <kelvykellysson@alu.ufc.br>
2023-08-25 21:53:18 -0300

    start of a study with colors and wheeling the page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="bg-blue-800 font-bold main flex flex-col justify-between ">
+      <div className='grid grid-cols-2 flex justify-between'>
+        <div> FAKE BACKSTAGE </div>
+        <div className='flex justify-end'>fake@backstagetals.com</div>
+      </div>
+      <div className='book'></div>
+      <div className='infos grid grid-cols-3 flex'>
+        <div className='flex'>Backstage Talks is a magazine of casual, but in depth dialogues on design and business. Our decisions shape and influence this complex world—to have a chance to make the right ones, we need to talk.
+© 2023 Published by Büro Milk
+
+Privacy Policy</div>
+        <div className='flex'>Issue #1 is sold out.
+
+If you are lucky, you may get the last pieces in selected stores.</div>
+      
+        <div className='flex'>fake@backstagetalks.com</div>
+      </div>
+    </main>
   );
 }
 
